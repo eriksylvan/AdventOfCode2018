d6903ae8a2c8796332f1f8818786c2405740185e
Sylvan <sylver@corp.delaval.com>
2020-11-16 23:28:28 +0100

    WIP day 7

--- src/main/Day_07.java
+++ src/main/Day_07.java
@@ -1,6 +1,11 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -11,6 +16,11 @@ import java.util.regex.Pattern;
 public class Day_07 {
 
     String inputFile = "input\\input_07.txt";
+
+    public static class Node {
+		public char name;
+        public List<Node> assambleAfter = new ArrayList<>();
+	}
 
     public ArrayList<String> getInputData() {
         Scanner inputScanner;
@@ -24,6 +34,12 @@ public class Day_07 {
     }
 
     public int day07PartOne(ArrayList<String> input) {
+        
+
+        List<Node> graph = new ArrayList<>(); 
+        Map<String, Integer> order = new HashMap<String, Integer>();
+        
+        ArrayList<String>  instruction = new ArrayList<String>();  
         for (String str : input) {
             Pattern p = Pattern.compile("Step ([A-Z]) must be finished before step ([A-Z])");
             Matcher m = p.matcher(str);
@@ -33,8 +49,27 @@ public class Day_07 {
                 step = m.group(1);
                 before = m.group(2);
             }
+            instruction.add(before + step);
+            if (order.containsKey(step)) {
+                order.put(step, order.get(step) + 1);
+            } else {
+                order.put(step, 1);
+            }
+            if (order.containsKey(before)) {
+                order.put(before, order.get(before) - 1);
+            } else {
+                order.put(before, -1);
+            }
             System.out.println(step + " -> " + before);
+        
         }
+        Collections.sort(instruction);
+        
+        System.out.println(instruction);
+
+        System.out.println(order);
+        Node n = new Node();
+
         System.out.println("Size: " + input.size());
         return 0;
     }
