0eda68c76afe61fcded7830088d8d75bab111109
Sylvan <sylver@corp.delaval.com>
2020-11-25 00:19:42 +0100

    Solution Day 11 part one

--- src/main/Day_11.java
+++ src/main/Day_11.java
@@ -21,9 +21,45 @@ public class Day_11 {
         return inp;
     }
 
-    public int day11PartOne() {
-        int sum = 0;
-        return sum;
+    public int getPowerLevel(int x, int y, int sn) {
+        int r = x + 10;
+        int pl = r * y;
+        pl = pl + sn;
+        pl = pl * r;
+        pl = (pl / 100) % 10;
+        pl = pl - 5;
+
+        return pl;
+    }
+
+    public String day11PartOne(int serial) {
+        // int[][] grid = new int[300][300];
+        int[][] grid = new int[300][300];
+
+        for (int y = 0; y < 300; y++) {
+            for (int x = 0; x < 300; x++) {
+                grid[x][y] = getPowerLevel(x, y, serial);
+
+            }
+        }
+
+        int maxP = Integer.MIN_VALUE;
+        int maxX = -1, maxY = -1;
+        for (int y = 0; y < 298; y++) {
+            for (int x = 0; x < 298; x++) {
+                //System.out.print(grid[x][y] + " ");
+                int p = grid[x][y] + grid[x+1][y] + grid[x+2][y] +
+                grid[x][y+1] + grid[x+1][y+1] + grid[x+2][y+1] +
+                grid[x][y+2] + grid[x+1][y+2] + grid[x+2][y+2];
+                if (p > maxP){
+                    maxP = p;
+                    maxX = x;
+                    maxY = y;
+                }
+            }
+        }
+        String c = maxX+","+maxY;
+        return c;
     }
 
     public int day11PartTwo() {
@@ -33,12 +69,12 @@ public class Day_11 {
 
     public static void main(String[] args) {
         Day_11 day_11 = new Day_11();
-        int answer1, answer2;
-        //ArrayList<Integer> inp = day_xx.getInputData();
-        answer1 = day_11.day11PartOne();
-        
+        String answer1;
+        int answer2;
+        answer1 = day_11.day11PartOne(9995);
+
         answer2 = day_11.day11PartTwo();
-        System.out.println("\n\nAdvent of code 2018, Day 11\n");    
+        System.out.println("\n\nAdvent of code 2018, Day 11\n");
         System.out.println("Solution Part one: " + answer1);
         System.out.println("Solution Part two: " + answer2 + "\n\n");
     }
---
+++ src/test/Day_11_test.java
@@ -0,0 +1,72 @@
+package test;
+
+import junit.framework.TestCase;
+import main.Day_11;
+import org.junit.Test;
+
+public class Day_11_test extends TestCase {
+
+    main.Day_11 day_11 = new Day_11();
+
+
+    @Test
+    public void testPowerLevel_1() {
+        int expected = 4;
+        int actual = day_11.getPowerLevel(3, 5, 8);
+
+        //System.out.println((9876543 / 100) % 10);
+        
+        assertEquals("expected: " + expected + " actual: " + actual, expected, actual);
+    }
+
+    @Test
+    public void testPowerLevel_2() {
+        int expected = -5;
+        int actual = day_11.getPowerLevel(122,79, 57);
+        
+        assertEquals("expected: " + expected + " actual: " + actual, expected, actual);
+    }
+
+    @Test
+    public void testPowerLevel_3() {
+        int expected = 0;
+        int actual = day_11.getPowerLevel(217,196, 39);
+        
+        assertEquals("expected: " + expected + " actual: " + actual, expected, actual);
+    }
+
+    @Test
+    public void testPowerLevel_4() {
+        int expected = 4;
+        int actual = day_11.getPowerLevel(101,153, 71);
+        
+        assertEquals("expected: " + expected + " actual: " + actual, expected, actual);
+    }
+
+    
+
+    @Test
+    public void testPartOne_2() {
+        int serialNumber = 18;
+        String expected = "33,45";
+        String actual = day_11.day11PartOne(serialNumber);
+        assertEquals("expected: " + expected + " actual: " + actual, expected, actual);
+    }
+
+    @Test
+    public void testPartOne_3() {
+        int serialNumber = 42;
+        String expected = "21,61";
+        String actual = day_11.day11PartOne(serialNumber);
+        assertEquals("expected: " + expected + " actual: " + actual, expected, actual);
+    }
+
+    @Test
+    public void testSolutionOne() {
+        int serialNumber = 9995;
+        String expected = "33,45";
+        String actual = day_11.day11PartOne(serialNumber);
+        assertEquals("expected: " + expected + " actual: " + actual, expected, actual);
+    }
+}
+
